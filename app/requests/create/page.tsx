7403fe340ca218741de3202e4c50c3f2b6a40ef5
FabioSebs <sebsphotosofficial@gmail.com>
2024-10-23 20:35:01 +0700

    document mounting error

--- app/requests/create/page.tsx
+++ app/requests/create/page.tsx
@@ -1,22 +1,27 @@
-"use client"
+"use client";
 
-import CreateRequestForm from "@/components/client/CreateRequestForm"
+import { useEffect, useState } from "react";
+import CreateRequestForm from "@/components/client/CreateRequestForm";
+import Unauthenticated from "@/components/client/Unauthenticated";
 
 export default function Home() {
+  const [token, setToken] = useState<string | null>(null);
 
-    const getCookie = (name: string) => {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop()?.split(";").shift();
-        return null;
-    };
+  const getCookie = (name: string) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(";").shift();
+    return null;
+  };
 
+  useEffect(() => {
     const token = getCookie("token");
+    setToken(token || '');
+  }, []);
 
-    return (
-        <div className='w-full flex flex-col justify-center items-center h-full'>
-            <CreateRequestForm token={token || ""} />
-        </div>
-    )
-
+  return (
+    <div className="w-full flex flex-col justify-center items-center h-full">
+      {token !== '' ? <CreateRequestForm token={token || ""} /> : <Unauthenticated />}
+    </div>
+  );
 }
