3002c8736730e989410f5a6fedee00c4ee6a8d51
FabioSebs <sebsphotosofficial@gmail.com>
2024-10-25 06:48:39 -0500

    client change

--- lib/http/client.ts
+++ lib/http/client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://64.31.42.142:4000/v1',
+    baseURL: 'https://icct-portal.fabrzy.dev/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
